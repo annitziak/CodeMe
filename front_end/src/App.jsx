c71c649e31e3dbe692f4de900080dcd5f539e5fb
evisUoE <E.Canga@sms.ed.ac.uk>
2025-02-12 18:54:38 +0000

    shadcn+tailwind setup

---
+++ front_end/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-500">
+        <h1 className="text-8xl">Hellooooo</h1>
+        <Button variant="outline">Button</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
