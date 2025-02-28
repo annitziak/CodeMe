bb149725d4f9748abcf174421282d600fca2242e
evisUoE <E.Canga@sms.ed.ac.uk>
2025-02-28 11:31:26 +0000

    added backend url in .env

--- front_end/src/features/searchApi.js
+++ front_end/src/features/searchApi.js
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 export const searchApi = createApi({
   reducerPath: "searchApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  // baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
   endpoints: (builder) => ({
-    
     search: builder.query({
       query: ({ query, page = 0, page_size = 10 }) => ({
         url: "/search",
--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -13,8 +13,9 @@ export default defineConfig({
   server: {
     proxy: {
       // Proxy API requests starting with /api to the Flask backend
-      '/api': {
-        target: 'http://127.0.0.1:8080', // Flask backend URL
+      "/api": {
+        // target: 'http://127.0.0.1:8080', // Flask backend URL
+        target: "http://35.226.202.157:8080", // backend url
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // Remove /api prefix
       },
